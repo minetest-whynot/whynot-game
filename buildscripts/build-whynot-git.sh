01288bea7c3265b0f596cff1f1e176ed8e2f586a
Alexander Weber <web.alexander@web.de>
2018-01-11 10:50:08 +0100

    build scripts updated, mod_sources.txt in main directiry now

--- buildscripts/build-whynot-git.sh
+++ buildscripts/build-whynot-git.sh
@@ -1,100 +1,126 @@
-#!/bin/sh
+#!/bin/bash
 
+
+# TODO1: GIT sources konsolidieren - 1x output pro Repo
+# TODO2: excluded berücksichtigen
 PROJ=~/Projekte/minetest-whynot-subgame-updater
 
 DST="$PROJ"/whynot-git/ # Subgame staging 
 
 # mods sources
 SRC="$PROJ"/mods_src/
-
 RSYNC="rsync -a --info=NAME --delete --exclude=.git --exclude=.gitignore"
+
+LOGFILE="$DST"/mod_sources.txt
+GIT="git  --no-pager"
+GITPARAM="--color=always"
+
+declare -a git_repositories
+function in_git_repositories {
+	for entry in "${git_repositories[@]}"; do
+		[[ "$entry" == "$1" ]] && echo "found";
+	done
+}
+
+function mod_install {
+
+	group=$1
+	shift
+
+	declare excluded
+	excluded_string=""
+	while (( "$#" )); do
+		if [ ${1:0:10} == '--exclude=' ]; then
+			excluded=(${excluded[@]} ${1#--exclude=*})
+			excluded_string="$excluded_string $1"
+			shift
+		else
+			break
+		fi
+	done
+#echo "excluded files:" ${excluded[@]}
+	function in_excluded {
+		for entry in "${excluded[@]}"; do
+			[[ "$entry" == "$1" ]] && echo "found";
+		done
+	}
+
+	declare sync_list
+	if [ "$#" == "0" ]; then
+		sync_list=("$group"/*)
+	else
+		sync_list=(${@})
+	fi
+#echo "sync_list:" ${sync_list[@]}
+
+## TODO: filter sync_list by excluded
+	for mod in ${sync_list[@]}; do
+		if [ -d "$mod" ] && [ -z "$(in_excluded "$(basename $mod)")" ]; then
+echo "Process repo $mod"
+			cd "$mod"
+			GIT_REPO="$(git remote -v | grep '\(fetch\)' )"
+			if [ -z "$(in_git_repositories "$GIT_REPO")" ]; then
+				echo '' >> "$LOGFILE"
+				echo "$GIT_REPO" >> "$LOGFILE"
+				git branch -vv | grep '^[*]' >> "$LOGFILE"
+				git_repositories=(${git_repositories[@]} "$GIT_REPO")
+			fi
+			echo "Mod: $mod" >> "$LOGFILE"
+			cd - >/dev/null
+ 			$RSYNC $excluded_string "$SRC"/"$mod" "$DST"/mods/"$group"/
+		fi
+	done
+	
+}
+
+################################
+cd "$SRC" # for proper resolving *
+rm "$LOGFILE" 2>/dev/null
 
 ## Sync minetest_game
 echo ' ##### sync minetest_game'
-$RSYNC --exclude=modpack.txt --exclude=farming "$SRC"/minetest_game/mods/* "$DST"/mods/minetest_game
-touch "$DST"/mods/minetest_game/modpack.txt
+mod_install minetest_game --exclude=farming minetest_game/mods/*
 
-echo ' ##### sync libs'
-$RSYNC "$SRC"/libs/* "$DST"/mods/libs/
+mod_install libs
+mod_install player
 
-echo ' ##### sync player mods'
-$RSYNC "$SRC"/player/* "$DST"/mods/player/
+mod_install ambience weather/* ambienceplus/*
 
-echo ' ##### sync weather / ambience'
-$RSYNC --exclude=modpack.txt "$SRC"/weather/* "$DST"/mods/ambience/
-$RSYNC "$SRC"/ambienceplus/* "$DST"/mods/ambience/
+mod_install player 3d_armor/3d_armor 3d_armor/3d_armor_stand 3d_armor/shields 3d_armor/wieldview
 
-echo ' ##### sync ambienceplus'
-$RSYNC --exclude=modpack.txt "$SRC"/ambienceplus/* "$DST"/mods/ambience/
+mod_install flora_ores
+mod_install mapgen
+mod_install tools
 
-echo ' ##### sync 3d_armor'
-$RSYNC "$SRC"/3d_armor/3d_armor "$DST"/mods/player/
-$RSYNC "$SRC"/3d_armor/3d_armor_stand "$DST"/mods/player/
-$RSYNC "$SRC"/3d_armor/shields "$DST"/mods/player/
-$RSYNC "$SRC"/3d_armor/wieldview "$DST"/mods/player/
+mod_install tools flight/flyingcarpet
 
-echo ' ##### sync flora+ores'
-$RSYNC "$SRC"/flora-ores/* "$DST"/mods/flora_ores/
+mod_install decor
+mod_install decor homedecor_modpack/homedecor_i18n
+mod_install decor homedecor_modpack/homedecor
+mod_install decor homedecor_modpack/building_blocks #grate and marble in recipes
+mod_install decor homedecor_modpack/chains
+mod_install decor homedecor_modpack/fake_fire
+mod_install decor homedecor_modpack/lavalamp
+mod_install decor homedecor_modpack/lrfurn
+mod_install decor homedecor_modpack/plasmascreen
 
-echo ' ##### sync mapgen'
-$RSYNC "$SRC"/mapgen/* "$DST"/mods/mapgen/
+mod_install decor mydoors/my_castle_doors
+mod_install decor mydoors/my_cottage_doors
+mod_install decor mydoors/my_default_doors
+mod_install decor mydoors/my_door_wood
+mod_install decor mydoors/my_fancy_doors
+mod_install decor mydoors/my_future_doors
+mod_install decor mydoors/my_hidden_doors
+mod_install decor mydoors/my_misc_doors
+mod_install decor mydoors/my_old_doors
+mod_install decor mydoors/my_old_doors
 
-echo ' ##### sync tools'
-$RSYNC "$SRC"/tools/* "$DST"/mods/tools/
-
-echo ' ##### sync flying tools'
-$RSYNC "$SRC"/flight/flyingcarpet "$DST"/mods/tools/
-
-echo ' ##### sync decor misc mods'
-$RSYNC "$SRC"/decor/* "$DST"/mods/decor/
-
-echo ' ##### sync decor_home pack'
-$RSYNC "$SRC"/homedecor_modpack/homedecor_i18n "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/homedecor "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/building_blocks "$DST"/mods/decor/ #grate and marble in recipes
-$RSYNC "$SRC"/homedecor_modpack/chains "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/fake_fire "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/lavalamp "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/lrfurn "$DST"/mods/decor/
-$RSYNC "$SRC"/homedecor_modpack/plasmascreen "$DST"/mods/decor/
-
-echo ' ##### sync mydoors pack'
-$RSYNC "$SRC"/mydoors/my_castle_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_cottage_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_default_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_door_wood "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_fancy_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_future_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_hidden_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_misc_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_old_doors "$DST"/mods/decor/
-$RSYNC "$SRC"/mydoors/my_old_doors "$DST"/mods/decor/
-
-echo ' ##### sync food'
-$RSYNC "$SRC"/food_modpack/food "$DST"/mods/food/
-$RSYNC "$SRC"/food_modpack/food_basic "$DST"/mods/food/
-$RSYNC "$SRC"/food/* "$DST"/mods/food/
+mod_install food food_modpack/food food_modpack/food_basic food/*
 
 ################### Game related files ########################
 echo ' ### Sync game related files'
-$RSYNC "$SRC"/whynot_compat "$DST"/mods/
-$RSYNC "$PROJ"/game_src/minetest.conf "$DST"/
-$RSYNC "$PROJ"/game_src/game.conf "$DST"/
-$RSYNC "$PROJ"/game_src/menu "$DST"/
-
-################### Create git logfile ########################
-echo '### Update git-sources.txt'
-GIT_LOGFILE="$DST"/buildscripts/git-sources.txt
-rm "$GIT_LOGFILE"
-touch "$GIT_LOGFILE"
-find "$SRC" -name ".git" -type d | while read repo; do
-   cd "$(dirname "$repo")"
-	git remote -v | grep fetch >> "$GIT_LOGFILE"
-	git branch -vv | grep '^[*]' >> "$GIT_LOGFILE"
-	echo '' >> "$GIT_LOGFILE"
- 
-   cd - >/dev/null
-done
-
-echo '### store used shell scripts'
+$RSYNC whynot_compat "$DST"/mods/
+$RSYNC  "$PROJ"/game_src/minetest.conf "$DST"/
+$RSYNC  "$PROJ"/game_src/game.conf "$DST"/
+$RSYNC  "$PROJ"/game_src/menu "$DST"/
 $RSYNC "$PROJ"/*.sh "$DST"/buildscripts
