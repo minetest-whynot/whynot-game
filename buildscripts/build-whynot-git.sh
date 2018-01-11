2fba65b8337b4fb4707d6d91397e1115d8ac92b4
Alexander Weber <web.alexander@web.de>
2018-01-11 12:05:35 +0100

    split build script functions to lib file; whitespace fixes

--- buildscripts/build-whynot-git.sh
+++ buildscripts/build-whynot-git.sh
@@ -1,84 +1,19 @@
 #!/bin/bash
 
-
-# TODO1: GIT sources konsolidieren - 1x output pro Repo
-# TODO2: excluded berücksichtigen
 PROJ=~/Projekte/minetest-whynot-subgame-updater
 
+SRC="$PROJ"/mods_src/  # mods sources
 DST="$PROJ"/whynot-git/ # Subgame staging 
-
-# mods sources
-SRC="$PROJ"/mods_src/
-RSYNC="rsync -a --info=NAME --delete --exclude=.git --exclude=.gitignore"
+LIB="$PROJ"/whynot-git/buildscripts/
 
 LOGFILE="$DST"/mod_sources.txt
-GIT="git  --no-pager"
-GITPARAM="--color=always"
-
-declare -a git_repositories
-function in_git_repositories {
-	for entry in "${git_repositories[@]}"; do
-		[[ "$entry" == "$1" ]] && echo "found";
-	done
-}
-
-function mod_install {
-
-	group=$1
-	shift
-
-	declare excluded
-	excluded_string=""
-	while (( "$#" )); do
-		if [ ${1:0:10} == '--exclude=' ]; then
-			excluded=(${excluded[@]} ${1#--exclude=*})
-			excluded_string="$excluded_string $1"
-			shift
-		else
-			break
-		fi
-	done
-#echo "excluded files:" ${excluded[@]}
-	function in_excluded {
-		for entry in "${excluded[@]}"; do
-			[[ "$entry" == "$1" ]] && echo "found";
-		done
-	}
-
-	declare sync_list
-	if [ "$#" == "0" ]; then
-		sync_list=("$group"/*)
-	else
-		sync_list=(${@})
-	fi
-#echo "sync_list:" ${sync_list[@]}
-
-## TODO: filter sync_list by excluded
-	for mod in ${sync_list[@]}; do
-		if [ -d "$mod" ] && [ -z "$(in_excluded "$(basename $mod)")" ]; then
-echo "Process repo $mod"
-			cd "$mod"
-			GIT_REPO="$(git remote -v | grep '\(fetch\)' )"
-			if [ -z "$(in_git_repositories "$GIT_REPO")" ]; then
-				echo '' >> "$LOGFILE"
-				echo "$GIT_REPO" >> "$LOGFILE"
-				git branch -vv | grep '^[*]' >> "$LOGFILE"
-				git_repositories=(${git_repositories[@]} "$GIT_REPO")
-			fi
-			echo "Mod: $mod" >> "$LOGFILE"
-			cd - >/dev/null
- 			$RSYNC $excluded_string "$SRC"/"$mod" "$DST"/mods/"$group"/
-		fi
-	done
-	
-}
-
-################################
-cd "$SRC" # for proper resolving *
 rm "$LOGFILE" 2>/dev/null
 
+
+source "$LIB"/build-whynot.lib
+cd "$SRC" # for proper resolving the '*'
+
 ## Sync minetest_game
-echo ' ##### sync minetest_game'
 mod_install minetest_game --exclude=farming minetest_game/mods/*
 
 mod_install libs
@@ -116,11 +51,3 @@ mod_install decor mydoors/my_old_doors
 mod_install decor mydoors/my_old_doors
 
 mod_install food food_modpack/food food_modpack/food_basic food/*
-
-################### Game related files ########################
-echo ' ### Sync game related files'
-$RSYNC whynot_compat "$DST"/mods/
-$RSYNC  "$PROJ"/game_src/minetest.conf "$DST"/
-$RSYNC  "$PROJ"/game_src/game.conf "$DST"/
-$RSYNC  "$PROJ"/game_src/menu "$DST"/
-$RSYNC "$PROJ"/*.sh "$DST"/buildscripts
--- buildscripts/src-update.sh
+++ buildscripts/src-update.sh
@@ -8,7 +8,7 @@ GIT="git  --no-pager"
 GITPARAM="--color=always"
 
 if  [ "$VERBOSE" == "" ]; then
-   VERBOSE=99
+	VERBOSE=99
 fi
 
 # 0: just update relevant infos
@@ -18,64 +18,64 @@ fi
 
 
 find . -name ".git" -type d | while read repo; do
-   echo ''
-   echo '--------------------------------------'
-   echo "$(dirname "$repo")"
-   echo '--------------------------------------'
+	echo ''
+	echo '--------------------------------------'
+	echo "$(dirname "$repo")"
+	echo '--------------------------------------'
 
-   cd "$(dirname "$repo")"
+	cd "$(dirname "$repo")"
 
 # fetch all connected remotes
-   for repo in $(git remote); do
-      $GIT fetch "$repo" --prune
-   done
+	for repo in $(git remote); do
+		$GIT fetch "$repo" --prune
+	done
 
 # Display repo status in different verbosity
-   if [ "$VERBOSE" -ge 1 ]; then
-      $GIT branch -vva $GITPARAM
-   fi
+	if [ "$VERBOSE" -ge 1 ]; then
+		$GIT branch -vva $GITPARAM
+	fi
 
 # create log range
-   currentbranch="$(git branch | grep '^*' | sed 's:\* ::g')"
-   remotebranch="$(git branch -avv | grep '^\*' | sed 's@.*\[@@g;s@[]:].*@@g' | grep -v '^\*')"
+	currentbranch="$(git branch | grep '^*' | sed 's:\* ::g')"
+	remotebranch="$(git branch -avv | grep '^\*' | sed 's@.*\[@@g;s@[]:].*@@g' | grep -v '^\*')"
 
 # print the log information
-   if [ "$VERBOSE" -ge 2 ]; then
-      if [ "$remotebranch" == "" ]; then
-         echo '!!!!! No remote branch for' $currentbranch '!!!!!!'
-      else
-         echo $currentbranch '=>' "$remotebranch"
-         $GIT log $currentbranch..$remotebranch
-      fi
+	if [ "$VERBOSE" -ge 2 ]; then
+		if [ "$remotebranch" == "" ]; then
+			echo '!!!!! No remote branch for' $currentbranch '!!!!!!'
+		else
+			echo $currentbranch '=>' "$remotebranch"
+			$GIT log $currentbranch..$remotebranch
+		fi
 
-      if [ "$VERBOSE" -ge 3 ]; then
-         $GIT remote -v
-      fi
-   fi
+		if [ "$VERBOSE" -ge 3 ]; then
+			$GIT remote -v
+		fi
+	fi
 
 # Now pull+merge
-#   git pull. (from man git-pull: git pull is shorthand for git fetch followed by git merge FETCH_HEAD.)
-   if [ ! "$remotebranch" == "" ]; then
-      $GIT merge "$remotebranch"
-   fi
+# git pull. (from man git-pull: git pull is shorthand for git fetch followed by git merge FETCH_HEAD.)
+	if [ ! "$remotebranch" == "" ]; then
+		$GIT merge "$remotebranch"
+	fi
 
 # display local changes
-   if [ $VERBOSE -ge 1 ]; then
-	   $GIT diff $GITPARAM #display local changes
-   fi
+	if [ $VERBOSE -ge 1 ]; then
+		$GIT diff $GITPARAM #display local changes
+	fi
 
-   echo "last commit: $(git log -1 --format=%cd)"
+	echo "last commit: $(git log -1 --format=%cd)"
 
-   cd - >/dev/null
+	cd - >/dev/null
 done
 
 
 #########################################
 # Find all ".hg" and pull them
 find . -name ".hg" -type d | while read repo; do
-   echo "$(dirname "$repo")"
-   cd "$(dirname "$repo")"
-   hg pull
-   cd - >/dev/null
+	echo "$(dirname "$repo")"
+	cd "$(dirname "$repo")"
+	hg pull
+	cd - >/dev/null
 done
 
