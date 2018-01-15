e856f39c6c06b02eeca6507a939bb7a0fd878aa5
Alexander Weber <web.alexander@web.de>
2018-01-15 21:03:21 +0100

    replaced carts by mesecons enabled boost_cart

---
+++ mods/tools/boost_cart/init.lua
@@ -0,0 +1,46 @@
+
+boost_cart = {}
+boost_cart.modpath = minetest.get_modpath("boost_cart")
+
+
+if not minetest.settings then
+	error("[boost_cart] Your Minetest version is no longer supported."
+		.. " (Version <= 0.4.15)")
+end
+
+local function getNum(setting)
+	return tonumber(minetest.settings:get(setting))
+end
+
+-- Maximal speed of the cart in m/s
+boost_cart.speed_max = getNum("boost_cart.speed_max") or 10
+-- Set to -1 to disable punching the cart from inside
+boost_cart.punch_speed_max = getNum("boost_cart.punch_speed_max") or 7
+-- Maximal distance for the path correction (for dtime peaks)
+boost_cart.path_distance_max = 3
+
+
+-- Support for non-default games
+if not default.player_attached then
+	default.player_attached = {}
+end
+
+minetest.after(0.5, function()
+	boost_cart.old_player_model = not minetest.global_exists("player_api")
+end)
+
+dofile(boost_cart.modpath.."/functions.lua")
+dofile(boost_cart.modpath.."/rails.lua")
+
+if minetest.global_exists("mesecon") then
+	dofile(boost_cart.modpath.."/detector.lua")
+--else
+--	minetest.register_alias("carts:powerrail", "boost_cart:detectorrail")
+--	minetest.register_alias("carts:powerrail", "boost_cart:detectorrail_on")
+end
+
+boost_cart.mtg_compat = minetest.global_exists("carts") and carts.pathfinder
+if boost_cart.mtg_compat then
+	minetest.log("action", "[boost_cart] Overwriting definitions of similar carts mod")
+end
+dofile(boost_cart.modpath.."/cart_entity.lua")
---
+++ mods/tools/boost_cart/rails.lua
@@ -0,0 +1,137 @@
+-- Common rail registrations
+
+local regular_rail_itemname = "default:rail"
+if minetest.registered_nodes["carts:rail"] then
+	-- MTG Compatibility
+	regular_rail_itemname = "carts:rail"
+end
+
+boost_cart:register_rail(":"..regular_rail_itemname, {
+	description = "Rail",
+	tiles = {
+		"carts_rail_straight.png", "carts_rail_curved.png",
+		"carts_rail_t_junction.png", "carts_rail_crossing.png"
+	},
+	groups = boost_cart:get_rail_groups()
+})
+
+-- Moreores' copper rail
+if minetest.get_modpath("moreores") then
+	minetest.register_alias("carts:copperrail", "moreores:copper_rail")
+else
+	boost_cart:register_rail(":carts:copperrail", {
+		description = "Copper rail",
+		tiles = {
+			"carts_rail_straight_cp.png", "carts_rail_curved_cp.png",
+			"carts_rail_t_junction_cp.png", "carts_rail_crossing_cp.png"
+		},
+		groups = boost_cart:get_rail_groups()
+	})
+
+	minetest.register_craft({
+		output = "carts:copperrail 12",
+		recipe = {
+			{"default:copper_ingot", "", "default:copper_ingot"},
+			{"default:copper_ingot", "group:stick", "default:copper_ingot"},
+			{"default:copper_ingot", "", "default:copper_ingot"},
+		}
+	})
+end
+
+-- Power rail
+boost_cart:register_rail(":carts:powerrail", {
+	description = "Powered rail",
+	tiles = {
+		"carts_rail_straight_pwr.png", "carts_rail_curved_pwr.png",
+		"carts_rail_t_junction_pwr.png", "carts_rail_crossing_pwr.png"
+	},
+	groups = boost_cart:get_rail_groups(),
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.get_meta(pos):set_string("cart_acceleration", "0.5")
+		end
+	end,
+	mesecons = {
+		effector = {
+			action_on = function(pos, node)
+				boost_cart:boost_rail(pos, 0.5)
+			end,
+			action_off = function(pos, node)
+				minetest.get_meta(pos):set_string("cart_acceleration", "0")
+			end,
+		},
+	},
+})
+
+minetest.register_craft({
+	output = "carts:powerrail 6",
+	recipe = {
+		{"default:steel_ingot", "default:mese_crystal_fragment", "default:steel_ingot"},
+		{"default:steel_ingot", "group:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "default:mese_crystal_fragment", "default:steel_ingot"},
+	}
+})
+
+-- Brake rail
+boost_cart:register_rail(":carts:brakerail", {
+	description = "Brake rail",
+	tiles = {
+		"carts_rail_straight_brk.png", "carts_rail_curved_brk.png",
+		"carts_rail_t_junction_brk.png", "carts_rail_crossing_brk.png"
+	},
+	groups = boost_cart:get_rail_groups(),
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.get_meta(pos):set_string("cart_acceleration", "-0.3")
+		end
+	end,
+	mesecons = {
+		effector = {
+			action_on = function(pos, node)
+				minetest.get_meta(pos):set_string("cart_acceleration", "-0.3")
+			end,
+			action_off = function(pos, node)
+				minetest.get_meta(pos):set_string("cart_acceleration", "0")
+			end,
+		},
+	},
+})
+
+minetest.register_craft({
+	output = "carts:brakerail 6",
+	recipe = {
+		{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+		{"default:steel_ingot", "group:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+	}
+})
+
+boost_cart:register_rail("boost_cart:startstoprail", {
+	description = "Start-stop rail",
+	tiles = {
+		"carts_rail_straight_ss.png", "carts_rail_curved_ss.png",
+		"carts_rail_t_junction_ss.png", "carts_rail_crossing_ss.png"
+	},
+	groups = boost_cart:get_rail_groups(),
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.get_meta(pos):set_string("cart_acceleration", "halt")
+		end
+	end,
+	mesecons = {
+		effector = {
+			action_on = function(pos, node)
+				boost_cart:boost_rail(pos, 0.5)
+			end,
+			action_off = function(pos, node)
+				minetest.get_meta(pos):set_string("cart_acceleration", "halt")
+			end,
+		},
+	},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "boost_cart:startstoprail 2",
+	recipe = {"carts:powerrail", "carts:brakerail"},
+})
