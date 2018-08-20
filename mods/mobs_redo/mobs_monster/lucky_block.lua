b3fe3718a0f79803e46288751adb99addb7997ca
Alexander Weber <web.alexander@web.de>
2018-08-20 07:30:30 +0200

    update skinsdb (remove updaters) + mobs_monster

--- mods/mobs_redo/mobs_monster/lucky_block.lua
+++ mods/mobs_redo/mobs_monster/lucky_block.lua
@@ -1,7 +1,30 @@
 
 if minetest.get_modpath("lucky_block") then
 
+	local web = {name = "mobs:cobweb"}
+	local web_trap = {
+		size = {x = 3, y = 3, z = 3},
+		data = {
+			web, web, web,
+			web, web, web,
+			web, web, web,
+
+			web, web, web,
+			web, web, web,
+			web, web, web,
+
+			web, web, web,
+			web, web, web,
+			web, web, web,
+		},
+	}
+
+	lucky_block:add_schematics({
+		{"webtrap", web_trap, {x = 1, y = 0, z = 1}},
+	})
+
 	lucky_block:add_blocks({
+		{"sch", "webtrap", 1, true},
 		{"spw", "mobs:dungeon_master", 1, nil, nil, 3, "Billy"},
 		{"spw", "mobs:sand_monster", 3},
 		{"spw", "mobs:stone_monster", 3, nil, nil, 3, "Bob"},
