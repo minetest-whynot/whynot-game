62c86e10e1359fc59980633468877841551798e0
Olivier Dragon <odragon@protonmail.com>
2022-07-05 22:04:51 -0400

    Move configuration associative arrays to separate lib file

--- builder/lib-build-whynot.sh
+++ builder/lib-build-whynot.sh
@@ -1,28 +1,5 @@
-function process_update_mods {
-  #
-  # Modify as needed
-  # ================
-  # Associative list of repositories that point to non-default branch
-  #
-  declare -A BRANCHES=(
-    [minetest_game/minetest_game]=origin/stable-5 # Stay on stable version
-    [flora_ores/farming]=0b06c7cd450c5ec9a76b3c22a9c57f06e4f8a7c2 # freeze due to incompatibility with milk buckets
-  )
 
-  #
-  # Modify as needed
-  # ================
-  # Associative list of modpacks with excluded modules
-  #
-  declare -A EXCLUDED=(
-    [minetest_game/minetest_game]='--exclude=farming --exclude=env_sounds --exclude=mtg_craftguide'
-    [player/3d_armor]='--exclude=3d_armor_ip --exclude=3d_armor_ui'
-    [mesecons/mesecons]='--exclude=mesecons_lucacontroller --exclude=mesecons_commandblock --exclude=mesecons_detector --exclude=mesecons_fpga --exclude=mesecons_gates --exclude=mesecons_hydroturbine --exclude=mesecons_luacontroller --exclude=mesecons_microcontroller --exclude=mesecons_stickyblocks'
-    [tools/flight]='--exclude=jetpack --exclude=wings'
-    [decor/homedecor_modpack]='--exclude=itemframes --exclude=homedecor_3d_extras --exclude=homedecor_inbox'
-    [decor/home_workshop_modpack]='--exclude=computers --exclude=home_workshop_machines'
-    [decor/mydoors]='--exclude=my_garage_door --exclude=my_saloon_doors --exclude=my_sliding_doors'
-  )
+function process_update_mods {
 
   local commit=$1
   local subm=$2
--- builder/update_mods.sh
+++ builder/update_mods.sh
@@ -22,7 +22,7 @@ cd "$SRC"
 >"$LOG"
 
 echo -n "Updating local repository..."
-git submodule sync
+git submodule sync --quiet
 git fetch --all --prune --prune-tags --tags --recurse-submodules=yes --quiet --job 4
 echo " done."
 echo -n "Updating submodules..."
@@ -30,6 +30,6 @@ git submodule update --init --recursive --quiet --jobs 4
 echo " done."
 
 echo "Process updates of submodules..."
-git submodule status | xargs -P 1 -n 3 bash -c 'process_update_mods "$@"' _
+git submodule status | xargs -P 1 -n 3 bash -c 'source "$PROJ"/builder/lib-config-whynot.sh; process_update_mods "$@"' _
 
 git commit -m "update mod_sources.txt" "$LOG"
