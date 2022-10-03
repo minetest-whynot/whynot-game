58e117d220c058b93beeda494bd2f19f48d8ee23
Olivier Dragon <odragon@protonmail.com>
2022-10-02 22:26:52 -0400

    Consider alternate license names
    and use README if no license file is found

--- builder/lib-build-whynot.sh
+++ builder/lib-build-whynot.sh
@@ -21,8 +21,16 @@ function sync_mods_folder {
     for childmod in `find $SRC/$subm -mindepth 1 -maxdepth 1 -type d`; do
       local childname=$(basename $childmod)
       if [[ ${childname:0:1} != "." && !( $exclusionlist =~ $childname$ || $exclusionlist =~ $childname[^[:alnum:]_] ) ]]; then
+
         $RSYNC $exclusionlist $childmod/ $DSTPATH/$childname/
-        cp [Ll][Ii][Cc][Ee][Nn][Ss][Ee]* $DSTPATH/$childname/
+        if ls $SRC/$subm/[Ll][Ii][Cc][Ee][Nn][SsCc][Ee]* &> /dev/null; then
+          cp --no-clobber $VERBOSEONLY $SRC/$subm/[Ll][Ii][Cc][Ee][Nn][SsCc][Ee]* $DSTPATH/$childname/
+        elif ls $SRC/$subm/[Cc][Oo][Pp][Yy][Ii][Nn][Gg]* &> /dev/null; then
+          cp --no-clobber $VERBOSEONLY $SRC/$subm/[Cc][Oo][Pp][Yy][Ii][Nn][Gg]* $DSTPATH/$childname/
+        elif ls $SRC/$subm/[Rr][Ee][Aa][Dd][Mm][Ee]* &> /dev/null; then
+          cp --no-clobber $VERBOSEONLY $SRC/$subm/[Rr][Ee][Aa][Dd][Mm][Ee]* $DSTPATH/$childname/
+        fi
+
       else
         rm -rf $VERBOSEONLY $DSTPATH/$childname
       fi
