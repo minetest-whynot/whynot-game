1742c8d42bda98ec5fa357845eaf8b78e3f7ab75
Olivier Dragon <odragon@protonmail.com>
2025-04-02 19:49:24 -0400

    Update mydoors from upstream.

--- mods/decor/my_default_doors/locked.lua
+++ mods/decor/my_default_doors/locked.lua
@@ -1,9 +1,13 @@
 local cdoor_list = {   --Number , Description , Inven Image , Image
-	{ "1", "Bronze Door" , "bronze", "bronze_ingot"},
-	{ "2", "Copper Door" , "copper", "copper_ingot"},
-	{ "3", "Gold Door" , "gold", "gold_ingot"},
-	{ "4", "Diamond Door" , "diamond", "diamond"},
-	{ "5", "Mese Door" , "mese", "mese_crystal"},
+	{ "1", "Bronze Door" , 	"bronze",		"bronze_ingot"},
+	{ "2", "Copper Door" , 	"copper",	 	"copper_ingot"},
+	{ "3", "Gold Door" , 	"gold", 	 	"gold_ingot"},
+	{ "4", "Diamond Door" , "diamond", 	 	"diamond"},
+	{ "5", "Mese Door" , 	"mese", 		"mese_crystal"},
+	{ "7", "Pine Wood" , 	"pine_wood", 	"pine_wood"},
+	{ "8", "Jungle Wood" ,	"jungle_wood",	"jungle_wood"},
+	{ "9", "Aspen Wood" , 	"aspen_wood",	"aspen_wood"},
+	{ "10", "Acacia Wood" ,	"acacia_wood",	"acacia_wood"},
 }
 
 local function add_door(num, desc, img, itm)
