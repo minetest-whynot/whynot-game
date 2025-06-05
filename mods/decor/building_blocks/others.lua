32e09017a306c28339795a98ee08dd5c7956940a
Olivier Dragon <odragon@protonmail.com>
2025-06-05 19:41:04 -0400

    Update homedecor_modpack from upstream.

--- mods/decor/building_blocks/others.lua
+++ mods/decor/building_blocks/others.lua
@@ -2,12 +2,12 @@ local S = minetest.get_translator("building_blocks")
 
 minetest.register_craftitem("building_blocks:sticks", {
 	description = S("Small bundle of sticks"),
-	image = "building_blocks_sticks.png",
+	inventory_image = "building_blocks_sticks.png",
 	on_place_on_ground = minetest.craftitem_place_item,
 })
 minetest.register_craftitem("building_blocks:tar_base", {
 	description = S("Tar base"),
-	image = "building_blocks_tar_base.png",
+	inventory_image = "building_blocks_tar_base.png",
 })
 
 minetest.register_tool("building_blocks:knife", {
